03348a461e160e0b76f7752f0b477063efe18ff0
Trinity-Armstrong <54957119+Trinity-Armstrong@users.noreply.github.com>
2019-09-24 14:49:06 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# created by: Trinity Armstrong
+# created on: September 2019
+# This program shows how local and global variables work
+
+# global variable
+variable_x = 25
+
+
+def local_variable():
+    # this shows what happens with local variables
+    
+    variable_X = 10
+    variable_Y = 30
+    variable_Z = variable_X + variable_Y
+    print("Local variables_X, variable_Y, variable_Z: {0} = {1} = {2}"
+    
+
+if __name__ == "__main__":
+    main()
